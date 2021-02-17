aa40e82786681d14bb2c3778313bc6607cac1702
Hamilton-junior <hamilton-souza@outlook.com.br>
2021-02-17 17:06:45 -0300

    modified features

--- public/script.js
+++ public/script.js
@@ -21,13 +21,26 @@ function countdown(event) {
   event.preventDefault();
   modal.classList.remove("show");
   try {
+    
     let date = form.elements[0].value;
     checkValue(date);
+    setInterval(() => {
     let dateBr = formatCurrency(date);
     dateBr = new Date(dateBr);
     let currentDate = dateUser();
-    currentDate = new Date(currentDate);
-    dataScreen(dateBr,currentDate);
+    
+    let valueDate = dateBr.getTime() - currentDate;
+
+      document.querySelector('[data-countdown="days"]').innerText = calcDay(
+        valueDate
+      );
+      document.querySelector('[data-countdown="hours"]').innerText =
+        calcHours(valueDate);
+      document.querySelector('[data-countdown="minutes"]').innerText =
+        calcMinutes(valueDate);
+      document.querySelector('[data-countdown="seconds"]').innerText =
+        calcSeconds(valueDate);
+    }, 1000);
   } catch (error) {
     alert(error.message);
   }
@@ -43,83 +56,70 @@ function checkValue(date) {
 // colocar o valor para data br
 function formatCurrency(date) {
   const splittedDate = date.split("-");
-  const month = months(splittedDate[1])
+  const month = months(splittedDate[1]);
   return `${month} ${splittedDate[2]} ${splittedDate[0]}`;
 }
 // pegar a data atual do usuario
 function dateUser() {
-    const now = new Date();
-    const month = months(`0${now.getMonth() + 1}`)
-    return `${now.getDate()} ${month} ${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+  const now = new Date();
+  return `${now.getTime()}`;
 }
 // função que vai pegar os meses em numeros e retornar uma string com o nome
 function months(value) {
-    let mes;
-    switch(value) {
-      case '01':
-          mes = 'January';
-          break;
-          case '02':
-          mes = 'February';
-          break;
-          case '03':
-          mes = 'March';
-          break;
-          case '04':
-          mes = 'April';
-          break;
-          case '05':
-          mes = 'May';
-          break;
-          case '06':
-          mes = 'June';
-          break;
-          case '07':
-          mes = 'July';
-          break;
-          case '08':
-          mes = 'August';
-          break;
-          case '09':
-          mes = 'September';
-          break;
-          case '10':
-          mes = 'October';
-          break;
-          case '11':
-          mes = 'November';
-          break;
-          case '12':
-          mes = 'December';
-          break;
+  let mes;
+  switch (value) {
+    case "01":
+      mes = "January";
+      break;
+    case "02":
+      mes = "February";
+      break;
+    case "03":
+      mes = "March";
+      break;
+    case "04":
+      mes = "April";
+      break;
+    case "05":
+      mes = "May";
+      break;
+    case "06":
+      mes = "June";
+      break;
+    case "07":
+      mes = "July";
+      break;
+    case "08":
+      mes = "August";
+      break;
+    case "09":
+      mes = "September";
+      break;
+    case "10":
+      mes = "October";
+      break;
+    case "11":
+      mes = "November";
+      break;
+    case "12":
+      mes = "December";
+      break;
     default:
-        mes = 'date';
-        break;
+      mes = "date";
+      break;
   }
   return mes;
 }
 // fazer o calculo para dias, horas, minutos, segundos
-function transformDay(day){
-    return day / (24 * 60 * 60 * 1000);
+function calcDay(value) {
+  return Math.floor(value / (24 * 60 * 60 * 1000));
 }
-function transformHours(hours) {
-    return hours / (60 * 60 * 1000);
+function calcHours(value) {
+  return Math.floor(value / (60 * 60 * 1000)) % 24;
 }
-function transformMinutes(minutes) {
-    return minutes / (60 * 1000);
+function calcMinutes(value) {
+  return Math.floor(value / (60 * 1000)) % 60;
 }
-function transformSeconds(seconds) {
-    return seconds / 1000;
-}
-// colocar na tela
-function dataScreen(dataOne, dateTwo) {
-    const spanDays = document.querySelector('[data-countdown="days"]');
-    const spanHours = document.querySelector('[data-countdown="hours"]');
-    const spanMinutes = document.querySelector('[data-countdown="minutes"]');
-    const spanSeconds = document.querySelector('[data-countdown="seconds"]');
-    spanDays.innerText = `${Math.round(transformDay(dataOne) - transformDay(dateTwo))}`;
-    spanHours.innerText = `${Math.round(transformHours(dataOne) - transformHours(dateTwo))}`;
-    spanMinutes.innerText = `${Math.round(transformMinutes(dataOne) - transformMinutes(dateTwo))}`;
-    spanSeconds.innerText = `${Math.round(transformSeconds(dataOne) - transformSeconds(dateTwo))}`;
-    
+function calcSeconds(value) {
+  return Math.floor(value / 1000) % 60;
 }
